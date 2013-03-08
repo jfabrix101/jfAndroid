59677bfe4d850bc1b15e8184e55805b4837b1cac
jfabrix101 <fabrizio.russo@gmail.com>
2013-03-08 19:49:16 +0100

    Added method to execute an http request in http or https

--- library/src/jfabrix101/lib/helper/NetworkHelper.java
+++ library/src/jfabrix101/lib/helper/NetworkHelper.java
@@ -19,13 +19,35 @@
 package jfabrix101.lib.helper;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import jfabrix101.lib.security.ssl.SimpleSSLSocketFactory;
 import jfabrix101.lib.util.Logger;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerPNames;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.util.EntityUtils;
 
 import android.content.Context;
@@ -34,6 +56,7 @@ import android.net.NetworkInfo.State;
 
 public class NetworkHelper {
 
+	private static Logger mLogger = Logger.getInstance(NetworkHelper.class);
 	
 	/**
 	 * Check the internet connection status.
@@ -91,22 +114,6 @@ public class NetworkHelper {
 	
 	
 	
-	public static String getUrl(String url) throws Exception {
-		HttpClient client = new DefaultHttpClient();
-		HttpGet getHttp = new HttpGet(url);
-		HttpResponse response = client.execute(getHttp);
-		int returnCode = response.getStatusLine().getStatusCode();
-		
-		String htmlBody = EntityUtils.toString(response.getEntity());
-		
-		if(returnCode != HttpStatus.SC_OK) {
-			Logger mLogger = Logger.getInstance(NetworkHelper.class);
-			mLogger.error("UpdateServiceHelper() - Network error reading URL: %s", url);
-			mLogger.error("UpdateServiceHelper() - Network error: HttpStatusCode : %s", response.getStatusLine().getStatusCode());
-			return null;
-		}
-		return htmlBody;
-	}
 	
 	/**
 	 * Download content from a URL in binary format.
@@ -139,11 +146,89 @@ public class NetworkHelper {
 		byte[] result = EntityUtils.toByteArray(response.getEntity());
 		
 		if(returnCode != HttpStatus.SC_OK) {
-			Logger mLogger = Logger.getInstance(NetworkHelper.class);
 			mLogger.error("UpdateServiceHelper() - Network error reading URL %s",url);
 			mLogger.error("UpdateServiceHelper() - Network error: HttpStatusCode : %s", response.getStatusLine().getStatusCode());
 			return null;
 		}
 		return result;
+	}
+	
+	/**
+	 * First step for an execution of an http request.
+	 * Create an HttpClient using http or https.
+	 * @return
+	 */
+	private static HttpClient getHttpClient() {
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+		schemeRegistry.register(new Scheme("https", new SimpleSSLSocketFactory(), 443));
+
+		HttpParams params = new BasicHttpParams();
+		params.setParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 30);
+		params.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE, new ConnPerRouteBean(30));
+		params.setParameter(HttpProtocolParams.USE_EXPECT_CONTINUE, false);
+		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+
+		ClientConnectionManager cm = new SingleClientConnManager(params, schemeRegistry);
+		HttpClient httpClient = new DefaultHttpClient(cm, params);
+		return httpClient;
+	}
+	
+	/**
+	 * Make an http request using the GET protocol
+	 * @param url
+	 * @return
+	 * @throws Exception
+	 */
+	public static String getUrl(String url) throws Exception {
+		HttpClient httpClient = getHttpClient();
+		HttpGet httpRequest = new HttpGet(url);
+		return execHttpRequest(httpClient, httpRequest);
+	}
+	
+	/**
+	 * Make an http request using the POST protocol
+	 * @param url 
+	 * @param params - Maps with parameters 
+	 * @return
+	 * @throws Exception
+	 */
+	public static String postUrl(String url, Map<String, String> params) throws Exception {
+		HttpClient httpClient = getHttpClient();
+		HttpPost httpRequest = new HttpPost(url);
+		
+		// Add post data
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(params.size());
+        for (Entry<String, String> entry : params.entrySet()) {
+        	String key = entry.getKey();
+        	String val = entry.getValue();
+        	nameValuePairs.add(new BasicNameValuePair(key, val));
+        }
+        httpRequest.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+		return execHttpRequest(httpClient, httpRequest);
+	}
+	
+	
+	/**
+	 * Last part of execution an http request.
+	 * Execute a request and return the body. 
+	 * @param httpClient
+	 * @param httpRequest
+	 * @return
+	 * @throws Exception
+	 */
+	private static String execHttpRequest(HttpClient httpClient, HttpRequestBase httpRequest)
+	throws Exception {
+		HttpResponse response = httpClient.execute(httpRequest);
+		int returnCode = response.getStatusLine().getStatusCode();
+		
+		String htmlBody = EntityUtils.toString(response.getEntity());
+		
+		if(returnCode != HttpStatus.SC_OK) {
+			mLogger.error( "- Network error reading URL: " + httpRequest.getURI().toString());
+			mLogger.error( "- Network error: HttpStatusCode : " + response.getStatusLine().getStatusCode());
+			return null;
+		}
+		return htmlBody;
 	}
 }
